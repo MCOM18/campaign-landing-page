8a99c3b9245a0e61b5a42556229440e1bbb02784
Sunil-Suthar-JOJO <sunilsuthar@appjojo.in>
2026-07-01 15:29:19 +0530

    fix: update next.config output logic to prevent static export in test and production environments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.NODE_ENV === "development" ? undefined : "export",
+  output: process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test" ? undefined : "export",
 };
 
 export default nextConfig;
