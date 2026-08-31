0d75e726860e93d64e087c51e8a6fb5d4f563346
EC2 Default User <ec2-user@ip-172-31-44-10.ap-south-1.compute.internal>
2026-08-31 12:26:14 +0000

    Updated deployment files

--- script/deploy.sh
+++ script/deploy.sh
@@ -24,33 +24,6 @@ node -v
 echo "NPM Version:"
 npm -v
 
-echo "========================================="
-echo "Reading Build Version"
-echo "========================================="
-
-if [ ! -f build_version.txt ]; then
-    echo "ERROR: build_version.txt not found!"
-    exit 1
-fi
-
-VERSION=$(cat build_version.txt)
-
-if [ -z "$VERSION" ]; then
-    echo "ERROR: Version is empty!"
-    exit 1
-fi
-
-echo "Build Version : $VERSION"
-
-echo
-
-
-if grep -q "^NEXT_PUBLIC_APP_VERSION=" .env.production; then
-    sed -i "s/^NEXT_PUBLIC_APP_VERSION=.*/NEXT_PUBLIC_APP_VERSION=${VERSION}/" .env.production
-else
-    echo "NEXT_PUBLIC_APP_VERSION=${VERSION}" >> .env.production
-fi
-
 echo "========================================"
 echo "Installing Dependencies"
 echo "========================================"
@@ -61,8 +34,7 @@ echo "========================================"
 echo "Building Next.js"
 echo "========================================"
 
-NODE_OPTIONS="--max-old-space-size=4096" \
-npm run build:prod -- --version="$VERSION"
+npm run build
 
 echo "========================================"
 echo "Deployment Completed"
